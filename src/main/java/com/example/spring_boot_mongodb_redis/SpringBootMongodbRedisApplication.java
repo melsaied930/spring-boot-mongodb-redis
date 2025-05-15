bf33f57a300c41f318799a3ae25a229aecb1fec5
Mohamed Elsayed <m.elsaied.930@gmail.com>
2025-05-15 13:20:37 +0300

    Create compose.yaml

---
+++ src/main/java/com/example/spring_boot_mongodb_redis/SpringBootMongodbRedisApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_mongodb_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongodbRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongodbRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_mongodb_redis/SpringBootMongodbRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_mongodb_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongodbRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
